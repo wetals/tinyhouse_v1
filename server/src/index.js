a94e037d49f24d4d59e1ef64dde96ac5aa4518ba
thanawatgulati <thanawatgulatijames@gmail.com>
2020-03-06 23:30:14 +0700

    🏡 Tinyhouse First commit

---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 9000;
+
+app.get('/',(req,res)=>{
+    res.send("Hello MyWorld!");
+})
+
+app.listen(port); 
+console.log(`[ app ] : http://localhost:/${port}`);
