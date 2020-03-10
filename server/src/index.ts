05cd457e9339ccd49eea9994cccd49d8715d1bd2
thanawatgulati <thanawatgulatijames@gmail.com>
2020-03-11 01:28:17 +0700

    🃏 GraphQL Playground & Schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,12 @@
-import { listings } from './listings';
+import { schema } from './graphql';
+import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
-import bodyParser from 'body-parser';
+
 const app = express();
 const port = 9000;
 
-app.use(bodyParser.json());
-
-app.get('/listings', (_req, res) => {
-  return res.send(listings);
-});
-
-app.post('/delete-listing', (req, res) => {
-  const id: string = req.body.id;
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-  res.send('failed to delete listing');
-});
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: '/api' });
 
 app.listen(port);
 console.log(`[ app ] : http://localhost:/${port}`);
