6ba695959a276a6bbadbea35d70fc37424ef1055
thanawatgulati <thanawatgulatijames@gmail.com>
2020-03-10 14:07:37 +0700

    🌞 Mock Listings and delete

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,23 @@
+import { listings } from './listings';
 import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
 const port = 9000;
 
-app.get('/', (_req, res) => {
-  res.send('Hello MyWorld!');
+app.use(bodyParser.json());
+
+app.get('/listings', (_req, res) => {
+  return res.send(listings);
+});
+
+app.post('/delete-listing', (req, res) => {
+  const id: string = req.body.id;
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+  res.send('failed to delete listing');
 });
 
 app.listen(port);
