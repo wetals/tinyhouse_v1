d2b798de859710ed33240f9d39ca8095ef95a28b
thanawatgulati <thanawatgulatijames@gmail.com>
2020-03-11 19:58:48 +0700

    🍃 Connect Mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,23 @@
+require('dotenv').config();
+import { connectDatabase } from './database';
 import { typeDefs, resolvers } from './grapgql';
 import { ApolloServer } from 'apollo-server-express';
-import express from 'express';
+import express, { Application } from 'express';
 
-const app = express();
-const port = 9000;
+const port = process.env.PORT;
 
-const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app, path: '/api' });
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db })
+  });
+  server.applyMiddleware({ app, path: '/api' });
 
-app.listen(port);
-console.log(`[ app ] : http://localhost:/${port}`);
+  app.listen(port);
+
+  console.log(`[ app ] : http://localhost:/${port}`);
+};
+
+mount(express());
