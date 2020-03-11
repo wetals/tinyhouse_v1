1239f1f32f10cc417b17df44b3be541b29959c6c
thanawatgulati <thanawatgulatijames@gmail.com>
2020-03-11 16:30:54 +0700

    ⚛ GraphQL Schema Listings

--- server/src/graphql.ts
+++ server/src/graphql.ts
@@ -1,11 +1,37 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+import { listings } from './listings';
+import {
+  GraphQLID,
+  GraphQLNonNull,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString
+} from 'graphql';
+
+const Listing = new GraphQLObjectType({
+  name: 'Listing',
+  fields: {
+    id: { type: GraphQLNonNull(GraphQLID) },
+    title: { type: GraphQLNonNull(GraphQLString) },
+    image: { type: GraphQLNonNull(GraphQLString) },
+    address: { type: GraphQLNonNull(GraphQLString) },
+    price: { type: GraphQLNonNull(GraphQLInt) },
+    numOfGuests: { type: GraphQLNonNull(GraphQLInt) },
+    numOfBeds: { type: GraphQLNonNull(GraphQLInt) },
+    numOfBaths: { type: GraphQLNonNull(GraphQLInt) },
+    rating: { type: GraphQLNonNull(GraphQLInt) }
+  }
+});
 
 const query = new GraphQLObjectType({
   name: 'Query',
   fields: {
-    hello: {
-      type: GraphQLString,
-      resolve: () => 'Hello from the Query'
+    listings: {
+      type: GraphQLNonNull(GraphQLList(GraphQLNonNull(Listing))),
+      resolve: () => {
+        return listings;
+      }
     }
   }
 });
@@ -13,9 +39,18 @@ const query = new GraphQLObjectType({
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
-    hello: {
-      type: GraphQLString,
-      resolve: () => 'Hello from the Mutation'
+    deleteListing: {
+      type: GraphQLNonNull(Listing),
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) }
+      },
+      resolve: (_root, { id }) => {
+        for (let i = 0; i < listings.length; i++) {
+          if (listings[i].id === id) {
+            return listings.splice(i, 1)[0];
+          }
+        }
+      }
     }
   }
 });
