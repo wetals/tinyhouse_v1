e6be88448626e2ad4047d81fc8e9bb2562d54a13
thanawatgulati <thanawatgulatijames@gmail.com>
2020-03-13 02:26:51 +0700

    🥳 Fetch Data from QraphQL to ReactJS

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { server } from '../../lib/api';
+
+const LISTINGS = `
+    query Listings {
+        listings{
+            id
+            title
+            image
+            address
+            price
+            numOfGuests
+            numOfBaths
+            rating
+          }
+    }
+`;
+
 interface Props {
   title: string;
 }
-
 export const Listings = ({ title }: Props) => {
-  return <h2>{title}</h2>;
+  const fetchListings = async () => {
+    const { data } = await server.fetch({ query: LISTINGS });
+    console.log(data);
+  };
+  return (
+    <div>
+      <h1>{title}</h1>
+      <button onClick={fetchListings}>Query Listings</button>
+    </div>
+  );
 };
