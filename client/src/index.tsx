f8006eaa75bfc8cfceb4faecc866a5a388659a4c
thanawatgulati <thanawatgulatijames@gmail.com>
2020-03-13 01:41:14 +0700

    ✌️ React Props Listings

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css';
 import * as serviceWorker from './serviceWorker';
+import { Listings } from './sections';
 
-render(<div>Hello World</div>, document.getElementById('root'));
+render(
+  <Listings title="TinyHouse Listings" />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ client/src/sections/Listings/Listings.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+interface Props {
+  title: string;
+}
+
+export const Listings = ({ title }: Props) => {
+  return <h2>{title}</h2>;
+};
